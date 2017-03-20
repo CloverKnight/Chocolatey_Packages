cf841c84e4661678e2732f65fb029d237a78ade2
Alex Ireland <ireland.alex@yahoo.ca>
2017-03-20 07:22:36 -0600

    Package now uses an embedded zip file. Updated pakcage to version 1.10.2.

--- visualstudiocode.portable/tools/chocolateyinstall.ps1
+++ visualstudiocode.portable/tools/chocolateyinstall.ps1
@@ -1,19 +1,16 @@
-﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 
-$packageParameters = Get-PackageParameters
-$packageName       = 'visualstudiocode.portable' # arbitrary name for the package, used in messages
-$url               = 'https://github.com/Microsoft/vscode/archive/1.9.1.zip'
-$unzipDir          = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$pp                = Get-PackageParameters
+$packageName       = 'visualstudiocode.portable'
+$zipPath           = Join-Path $(Split-Path $MyInvocation.MyCommand.Path) 'VSCode1.10.2.zip'
+$unzipDir 		   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
-if ($packageParameters[InstallationPath])
-{
-	$unzipDir = $packageParameters[InstallationPath]
-}
+if($pp["InstallationPath"]) { $unzipDir = $pp["InstallationPath"] }
+
 $packageArgs = @{
   packageName   = $packageName
-  unzipLocation = $unzipDir
-  url           = $url
-  checksum      = '4F108C8F1296233A516A6584DDCD3E7EC1CDC0A062FE1158307D110C9EC204D5'
-  checksumType  = 'sha256'
+  destination   = $unzipDir
+  fileFullPath  = $zipPath
+}
 
-Install-ChocolateyZipPackage @packageArgs
+Get-ChocolateyUnzip @packageArgs
