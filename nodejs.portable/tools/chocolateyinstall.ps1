be23080a4419b2cc7d2b496da25c9920e3c2212a
Alex Ireland <ireland.alex@yahoo.ca>
2017-07-23 15:31:26 -0600

    Removed unused files from templated packages. Updated VSCode portable package.

--- nodejs.portable/tools/chocolateyinstall.ps1
+++ nodejs.portable/tools/chocolateyinstall.ps1
@@ -2,18 +2,18 @@
 
 $pp                = Get-PackageParameters
 $packageName       = 'nodejs.portable'
-$unzipDir 		   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$unzipDir 		     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $url32 = https://nodejs.org/dist/v6.10.2/node-v6.10.2-win-x86.zip 
 $url64 = https://nodejs.org/dist/v6.10.2/node-v6.10.2-win-x64.zip 
 
 if($pp["InstallationPath"]) { $unzipDir = $pp["InstallationPath"] }
-
+        
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $unzipDir
-  url 			= $url32
-  url64bit		= $url64
+  url           = $url32
+  url64bit		  = $url64
 }
 
 Install-ChocolateyZipPackage @packageArgs
--- visualstudiocode.portable/tools/chocolateyinstall.ps1
+++ visualstudiocode.portable/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $pp                = Get-PackageParameters
 $packageName       = 'visualstudiocode.portable'
-$zipPath           = Join-Path $(Split-Path $MyInvocation.MyCommand.Path) 'VSCode1.10.2.zip'
-$unzipDir 		   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$zipPath           = Join-Path $(Split-Path $MyInvocation.MyCommand.Path) 'VSCode-win32-ia32-1.14.2.zip'
+$unzipDir 		     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 if($pp["InstallationPath"]) { $unzipDir = $pp["InstallationPath"] }
 
